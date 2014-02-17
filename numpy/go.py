59a865e20ecf5fa6c55bb697b0f657cf2a7f406f
=Nelson Auner <ne.auner@gmail.com>
2014-02-17 15:07:28 -0600

    numpy function

---
+++ numpy/go.py
@@ -0,0 +1,11 @@
+import numpy
+import ctypes
+indata = numpy.ones((5,6), dtype=numpy.double)
+outdata = numpy.zeros((5,6), dtype=numpy.double)
+lib = ctypes.cdll.LoadLibrary('./ctest.so')
+fun = lib.cfun
+# Here comes the fool part.
+fun(ctypes.c_void_p(indata.ctypes.data), ctypes.c_void_p(outdata.ctypes.data))
+
+print 'indata: %s' % indata
+print 'outdata: %s' % outdata
